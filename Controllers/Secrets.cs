452a51ee8fbae89bc6390a0e51df4362ee9844b2
Almanac <ruoyw0@gmail.com>
2021-03-21 20:54:28 -0700

    Added in Secrets static class to hold sensitive information such as API keys. Will have to make Google Map API key public.

---
+++ Controllers/Secrets.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Controllers
+{
+    public static class Secrets
+    {
+
+    }
+}
