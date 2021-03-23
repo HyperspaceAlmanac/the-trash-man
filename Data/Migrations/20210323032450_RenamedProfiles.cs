79ff8f4334f68a9a19a02de5be66f6c566204883
Almanac <ruoyw0@gmail.com>
2021-03-22 20:33:29 -0700

    Code can compile again... Added in a huge monstrocity to display all of the pickups in area. Need to look at past commits to find out what I did for button to hide map until needed

---
+++ Data/Migrations/20210323032450_RenamedProfiles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class RenamedProfiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
