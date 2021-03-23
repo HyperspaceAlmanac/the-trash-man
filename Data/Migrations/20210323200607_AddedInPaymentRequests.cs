ebcbb04c7d5cf4131caf0e4aeef58a073bea295f
Almanac <ruoyw0@gmail.com>
2021-03-23 13:06:42 -0700

    Applied Database Migration

---
+++ Data/Migrations/20210323200607_AddedInPaymentRequests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class AddedInPaymentRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SessionID = table.Column<string>(nullable: true),
+                    PickupId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaymentRequests_CompletedPickups_PickupId",
+                        column: x => x.PickupId,
+                        principalTable: "CompletedPickups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentRequests_PickupId",
+                table: "PaymentRequests",
+                column: "PickupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentRequests");
+        }
+    }
+}
